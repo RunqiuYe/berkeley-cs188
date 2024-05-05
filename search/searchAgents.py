cba79ccf3750d13f3de74d7ffd10dc3c6fd12d78
Runqiu_Ye <ye_runqiu@outlook.com>
2024-05-04 21:43:36 -0400

    finished suboptimal search (greedy)

--- search/searchAgents.py
+++ search/searchAgents.py
@@ -629,7 +629,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.uniformCostSearch(problem)
 
 
 class AnyFoodSearchProblem(PositionSearchProblem):
@@ -666,7 +666,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x, y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 
 def mazeDistance(
